efbcfb67df07c43db2af573fc671abb5632d187b
Yi Jin <yi.jin@riotinto.com>
2021-03-21 23:04:46 -0400

    add UA

--- template/ga_script.js
+++ template/ga_script.js
@@ -1,5 +1,6 @@
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 
 gtag('config', 'G-QEJH2B8L8V');
+gtag('config', 'UA-188253941-1');
